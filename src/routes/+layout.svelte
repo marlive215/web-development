1e9da626ea2e9f094f5a88f635da4628574a1677
Amara <amaradonzomd@gmail.com>
2023-09-13 00:50:56 -0400

    I added the the Agency logo, but have hard time trying to place the logo on the same line as the page links. I also want to push the logo all the way to the left

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,12 @@
 
 </script>
 <main>
-    <head>
-        <!-- Included link to Google font but does not show up on website for nav links, font in body -->
-        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-    </head>
     <body class="page-body">
         <div class="container">
             <nav class="navbar">
-                <!-- Where the logo and/or agency name should go -->
-                <h1>Agency Name</h1>
-                <!-- Links for the navbar -->
+                <div class="logo">
+                    <h1 class="agency-name"><a href="/">SpyderByte</a></h1>
+                </div>
                 <div class="menu-options">
                     <ul class="list-of-link">
                         <li><a href="/">Home</a></li>
@@ -22,9 +18,19 @@
                     </ul>
                 </div>
             </nav>
-        </div>    
+            <div class="content">
+                <div class="url-social-links">
+                    <ul class="social-link">
+                        <li><a class="social-icons" href="https://www.facebook.com/"><img src="https://img.icons8.com/ios-filled/50/ffffff/facebook.png" width="25" height="25" /></a></li>
+                        <li><a class="social-icons" href="https://www.instagram.com/"><img src="https://img.icons8.com/ios-filled/50/ffffff/instagram-new.png" width="25" height="25" /></a></li>
+                        <li><a class="social-icons" href="https://www.twitter.com/"><img src="https://img.icons8.com/ios-filled/50/ffffff/twitter.png" width="25" height="25" /></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </body>
 </main>
+
 
 
 
@@ -40,7 +46,7 @@
         padding: 0;
         background: #000;
         /*  font won't chnage to Montserrat*/
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: 'Montserrat', sans-serif;
         font-size: 18px;
     }
 
@@ -51,11 +57,18 @@
         padding: 0px;
         background:black;
     }
-    .navbar {
+
+    /* .logo {
         margin-top: 40px;
+    } */
+    .navbar {
+        margin: 0;
+        margin-top: 40px;
+        flex-grow: 1;
         display: flex;
         background: black;
-        height: 50px;
+        height: 100px;
+        align-items: center;
 
         
 
@@ -77,21 +90,67 @@
 
     .list-of-link li {
         display: inline;
-        margin-right: 10px;
+        margin-right: 50px;
+
     }
     .menu-options li {
         display: inline;
-        margin: 0 10px; 
+        /* margin: 0 30px;  */
     }
 
     .list-of-link a {
+    text-decoration: none;
+    color: white;
+    font-size: 20px; 
+    font-family: 'Montserrat', sans-serif; 
+    font-weight: medium; 
+}
+
+    .url-social-links{
+        position: absolute;
+        right: 40px; 
+        bottom: 841.5px;
+        transform: translateY(-50%);
+        
+    }
+
+    .url-social-links a {
         text-decoration: none;
         color: white;
-        font-size: 20px;
     }
-    h1 {
-        font-size: 20px;
-        font-weight: normal;
-        color: #ffff;
-    }   
+
+    .url-social-links li {
+        display: inline;
+    }
+
+    .social-icons {
+        font-size: 24px;
+    }
+
+    .social-link {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        
+    }
+
+    .social-link li {
+        display: inline;
+        margin-left: 20px;
+    }
+
+    .agency-name a {
+        text-decoration: none;
+        font-size: 30px;
+        font-weight: bold;
+        color: #ffff; 
+       
+
+     }
+
+     .agency-name {
+        left: 4000px;
+     }
+
+  
 </style>
