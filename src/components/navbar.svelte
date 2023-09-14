7faf97deceac064b876f731373263ac0b3b46bcf
Amara <amaradonzomd@gmail.com>
2023-09-13 22:11:45 -0400

    added navbar component and added some code

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,14 +1,50 @@
-<nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Features</a>
-        <a class="nav-item nav-link" href="#">Pricing</a>
-        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+<nav class="navbar navbar-expand-md navbar-dark bg-black">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold" href="/">SpyderByte</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar menus -->
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/pricing">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/faq">FAQ</a>
+          </li>
+        </ul>
+        <!-- Social media icons -->
+        <ul class="navbar-nav sm-icons">
+          <li><a class="nav-link" href="/">lol<i class="bi bi-facebook"></i></a></li>
+          <li><a class="nav-link" href="/">hey<i class="bi bi-instagram"></i></a></li>
+          <li><a class="nav-link" href="/">yo<i class="bi bi-twitter"></i></a></li>
+          <li><a class="nav-link" href="/">dad<i class="bi bi-pinterest"></i></a></li>
+        </ul>
       </div>
     </div>
   </nav>
+  
+  <style>
+    .navbar {
+        background-color: black;
+    }
+    .navbar .navbar-nav .nav-link {
+    color: #ffffff;
+    }   
+    .sm-icons {
+    flex-direction: row;
+    }
+    .sm-icons .nav-link {
+    padding-right: 1em;
+    }
+  </style>
