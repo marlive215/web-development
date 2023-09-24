0c01dde25c552d9872180418c5f119bb9ca4a025
Dave Rolsky <autarch@urth.org>
2023-09-24 11:52:56 -0500

    Make navbar use array of links

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,50 +1,55 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-black">
-    <div class="container-fluid">
-      <a class="navbar-brand fw-bold" href="/">SpyderByte</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Navbar menus -->
-        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/pricing">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/faq">FAQ</a>
-          </li>
-        </ul>
-        <!-- Social media icons -->
-        <ul class="navbar-nav sm-icons">
-          <li><a class="nav-link" href="/">lol<i class="bi bi-facebook"></i></a></li>
-          <li><a class="nav-link" href="/">hey<i class="bi bi-instagram"></i></a></li>
-          <li><a class="nav-link" href="/">yo<i class="bi bi-twitter"></i></a></li>
-          <li><a class="nav-link" href="/">dad<i class="bi bi-pinterest"></i></a></li>
-        </ul>
-      </div>
+<script>
+    import { page } from '$app/stores';
+    let links = [
+        {
+            title: "Home",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+        {
+            title: "Pricing",
+            path: "/pricing",
+        },
+        {
+            title: "FAQ",
+            path: "/faq",
+        },
+    ];
+</script>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="/">SpyderByte</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Navbar menus -->
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        {#each links as link}
+        <li class="nav-item">
+          <a class="nav-link" class:active={link.path === $page.url.pathname} aria-current={link.path === $page.url.pathname} href="{link.path}">{link.title}</a>
+        </li>
+        {/each}
+      </ul>
+      <!-- Social media icons -->
+      <ul class="navbar-nav sm-icons">
+        <li>
+          <a class="icon-link" href="https://facebook.com/">
+            <svg class="bi" aria-hidden="true"><use xlink:href="#facebook"></use></svg>
+          </a>
+        </li>
+        <li><a class="nav-link" href="/">hey<i class="bi bi-instagram"></i></a></li>
+        <li><a class="nav-link" href="/">yo<i class="bi bi-twitter"></i></a></li>
+        <li><a class="nav-link" href="/">dad<i class="bi bi-pinterest"></i></a></li>
+      </ul>
     </div>
-  </nav>
-  
-  <style>
-    .navbar {
-        background-color: black;
-    }
-    .navbar .navbar-nav .nav-link {
-    color: #ffffff;
-    }   
-    .sm-icons {
-    flex-direction: row;
-    }
-    .sm-icons .nav-link {
-    padding-right: 1em;
-    }
-  </style>
+  </div>
+</nav>
